5948a1259568a07c58153bdfaaaaf9acac3188e4
rickchallen <wrf201@gmail.com>
2018-02-06 11:42:49 -0300

    Atualizando Classe de conexão

--- Locadora/Conexao.php
+++ Locadora/Conexao.php
@@ -1,4 +1,12 @@
 <?php
+class Conexao {
+
+    private $conexao = new PDO("mysql:host=localhost;dbname=locadora","root","130490");
+    
 
 
+    public function getConexao(){
+        return $this->$conexao->exec("set names utf8");
+    }
+}
 ?>
