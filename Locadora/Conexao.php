9f87f7a9bfd4d2747d71b4c50720de5bc10f577e
rickchallen <wrf201@gmail.com>
2018-02-19 11:45:18 -0300

    Método Gerador de conexão com o banco de dados finalizado

--- Locadora/Conexao.php
+++ Locadora/Conexao.php
@@ -3,13 +3,13 @@ class Conexao {
 
     private $conexao ="";
     
-    private function conect():PDO{
+    private function conect(){
         try{
            $conexao = new PDO("mysql:host=localhost;dbname=locadora;","root","130490");
            $conexao->exec("set names utf8");
            return $conexao;
         }catch(PDOException $e){
-            echo "Erro: ".$e->getMessage();
+            return $e->getMessage();
             exit();       
         }
     }
